f20a6d7d9310883a8aa3933c07a8c84428e8b71e
ArtemPedenko <nubostrel@gmail.com>
2023-12-18 18:42:05 +0300

    test

--- src/controllers/User.Controller.ts
+++ src/controllers/User.Controller.ts
@@ -10,7 +10,7 @@ class UserController {
     res.cookie("refreshToken", userData.refreshToken, {
       maxAge: 30 * 24 * 60 * 60 * 1000,
       httpOnly: true,
-      secure: false,
+      secure: true,
       sameSite: "none",
       path: "/",
     });
