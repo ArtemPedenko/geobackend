e6f84c12de6c183d3d79000ed68409b949a37b8a
ArtemPedenko <nubostrel@gmail.com>
2024-02-03 15:21:49 +0300

    new logic to name for files

--- src/controllers/ImageController.ts
+++ src/controllers/ImageController.ts
@@ -16,12 +16,8 @@ class ImageController {
   }
 
   async upload(req: CustomRequest, res: Response) {
-    console.log('upload');
-
-    const decodedToken = decodeToken(req.headers.cookie);
-    console.log('upload');
-    console.log(req.files.file);
-    const result = await service.upload(req.files.file, decodedToken.login);
+   const decodedToken = decodeToken(req.headers.cookie);
+    const result = await service.upload(req.files.file, req.body.alt,  decodedToken.login);
     return res.send(result);
   }
 
